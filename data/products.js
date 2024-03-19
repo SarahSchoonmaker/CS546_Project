6074b879bffcb0851eabd708f49cabba58798023
sarahschoonmaker <srschoonmaker@gmail.com>
2024-03-18 21:05:55 -0400

    project files

---
+++ data/products.js
@@ -0,0 +1,34 @@
+// This data file should export all functions using the ES6 standard as shown in the lecture code
+
+const create = async (
+  productName,
+  productDescription,
+  modelNumber,
+  price,
+  manufacturer,
+  manufacturerWebsite,
+  keywords,
+  categories,
+  dateReleased,
+  discontinued
+) => {};
+
+const getAll = async () => {};
+
+const get = async (productId) => {};
+
+const remove = async (productId) => {};
+
+const update = async (
+  productId,
+  productName,
+  productDescription,
+  modelNumber,
+  price,
+  manufacturer,
+  manufacturerWebsite,
+  keywords,
+  categories,
+  dateReleased,
+  discontinued
+) => {};
---
+++ data/reviews.js
@@ -0,0 +1,17 @@
+// This data file should export all functions using the ES6 standard as shown in the lecture code
+
+const createReview = async (
+  productId,
+  title,
+  reviewerName,
+  review,
+  rating
+) => {};
+
+const getAllReviews = async (productId) => {};
+
+const getReview = async (reviewId) => {};
+
+const updateReview = async (reviewId, updateObject) => {};
+
+const removeReview = async (reviewId) => {};
